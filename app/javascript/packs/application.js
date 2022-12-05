433c20068d949e118e4dd3e1da2c22f35d25bcc7
Dengz <dengweiyi870626@gmail.com>
2022-12-05 17:32:26 +0800

    fontawesome & Stimulus installed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,9 @@ import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 import "stylesheets/application";
-
+import "@fortawesome/fontawesome-free/css/all.css";
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
+
+import "controllers";
