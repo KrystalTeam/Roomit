049a07a0a777e944764e9bc001a975a79b77c00f
Dengz <dengweiyi870626@gmail.com>
2022-12-05 18:46:03 +0800

    header and roomcard

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,9 +1,6 @@
 // Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
-
-import { Application } from "stimulus";
-import { definitionsFromContext } from "stimulus/webpack-helpers";
-
 const application = Application.start();
-const context = require.context("controllers", true, /_controller\.js$/);
-application.load(definitionsFromContext(context));
+import { Application } from "@hotwired/stimulus";
+import Dropdown from "stimulus-dropdown";
+application.register("dropdown", Dropdown);
