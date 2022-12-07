160c252eb32b88618043aac81979d53d0090672c
kchsu <kevin3117213@gmail.com>
2022-12-07 15:34:26 +0800

    ignore .env.development; remove unused quotes; add phone_num, description, role and deleted_at to users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks', passwords: 'users/passwords', registrations: 'users/registrations', confirmations: 'users/confirmations', mailer: 'users/mailer', unlocks: 'users/unlocks' }
   root "room#home"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
