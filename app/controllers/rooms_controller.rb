1612b2016c9624b793e8389544d08a3cded2ee2a
Dengz <dengweiyi870626@gmail.com>
2022-12-09 17:04:59 +0800

    add RWD and borderline

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -12,9 +12,9 @@ class RoomsController < ApplicationController
   def create
     @room = Room.new(room_params)
     if @room.save
-      redirect_to rooms_path ,notice: '新增成功'
+      redirect_to rooms_path, notice: "新增成功"
     else
-      flash.alert = '新增失敗'
+      flash.alert = "新增失敗"
       render :new
     end
   end
@@ -27,16 +27,19 @@ class RoomsController < ApplicationController
 
   def update
     if @room.update(room_params)
-      redirect_to rooms_path, notice: '更新成功'
+      redirect_to rooms_path, notice: "更新成功"
     else
-      flash.alert = '更新失敗'
+      flash.alert = "更新失敗"
       render :new
     end
   end
 
   def destroy
     @room.update(deleted_at: Time.current)
-    redirect_to rooms_path, notice: '已刪除'
+    redirect_to rooms_path, notice: "已刪除"
+  end
+
+  def card
   end
 
   private
@@ -48,5 +51,4 @@ class RoomsController < ApplicationController
   def room_params
     params.require(:room).permit(:home_type, :room_type, :max_occupancy, :bedrooms, :bathrooms, :has_bathtub, :has_kitchen, :has_air_con, :has_wifi, :summary, :address, :price, :checkin_start_at, :checkin_end_at, :checkout_time)
   end
-
 end
