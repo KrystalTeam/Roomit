7f90c8eda83fab39720387a4ab3d10b4cce01081
kerker000 <kate00715@gmail.com>
2022-12-10 23:22:05 +0800

    add avatar attached to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :trackable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[ facebook google_oauth2 ]
+
+  has_one_attached :avatar
   
   enum role: %i[ guest host admin ]
 
