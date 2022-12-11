011693fa5fdf5299b01d57992577732cce94f6ad
kerker000 <kate00715@gmail.com>
2022-12-11 13:41:18 +0800

    conflict solve

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_09_041908) do
 ActiveRecord::Schema.define(version: 2022_12_09_084050) do
 
   # These are extensions that must be enabled in order to support this database
