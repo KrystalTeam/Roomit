ab7c86bc4f1d8ead21aa1ee5cb844e564319a666
Dengz <dengweiyi870626@gmail.com>
2022-12-11 23:02:55 +0800

    add zh-TW.yml,change time zone

--- config/application.rb
+++ config/application.rb
@@ -11,13 +11,14 @@ module Roomit
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = "zh-TW"
+    config.i18n.default_locale = "zh-CN"
+    config.time_zone = "Taipei"
 
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
