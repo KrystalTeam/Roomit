6881e2668b4e3bee054360cbd0cd9e10734d0a69
kchsu <kevin3117213@gmail.com>
2022-12-13 18:28:44 +0800

    gem duplicates deleted
    'conficts solved'

--- db/schema.rb
+++ db/schema.rb
@@ -179,6 +179,4 @@ ActiveRecord::Schema.define(version: 2022_12_13_030057) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "rooms"
   add_foreign_key "bookings", "users"
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
