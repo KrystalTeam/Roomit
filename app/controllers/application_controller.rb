96a177eabe49ef65c00b1de439452ffd669cda90
kchsu <kevin3117213@gmail.com>
2022-12-13 18:07:17 +0800

    unwanted test & css files deleted
    'conflicts solved'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 before_action :authenticate_user! 
 
@@ -10,4 +11,7 @@ before_action :authenticate_user!
 =======
     # before_action :authenticate_user!
 >>>>>>> 6739b6f (LINE Pay available)
+=======
+    before_action :authenticate_user!
+>>>>>>> 6daa6e3 (unwanted test & css files deleted)
 end
