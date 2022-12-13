d6eaf4fa81e8ba9fcf7ebf858f8a9db8956325c2
Dengz <dengweiyi870626@gmail.com>
2022-12-13 17:50:13 +0800

    add user edit name/email/number
    conflicts solved

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,8 +1,11 @@
 // Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
 const application = Application.start();
-import { Application } from "stimulus";
-import Dropdown from "stimulus-dropdown";
-application.register("dropdown", Dropdown);
-import Carousel from "stimulus-carousel";
-application.register("carousel", Carousel);
+import { Application } from 'stimulus';
+import Dropdown from 'stimulus-dropdown';
+application.register('dropdown', Dropdown);
+import Carousel from 'stimulus-carousel';
+application.register('carousel', Carousel);
+import { definitionsFromContext } from 'stimulus/webpack-helpers';
+const context = require.context('controllers', true, /_controller\.js$/);
+application.load(definitionsFromContext(context));
