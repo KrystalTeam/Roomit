e7512e65ac0817e915aaff38a695728584e53aeb
kchsu <kevin3117213@gmail.com>
2022-12-13 16:41:28 +0800

    LINE Pay available
    'conflicts solved'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
+<<<<<<< HEAD
 
 before_action :authenticate_user! 
 
+=======
+    # before_action :authenticate_user!
+>>>>>>> 8a22f19 (LINE Pay available)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2022_12_13_030057) do
+=======
+ActiveRecord::Schema.define(version: 2022_12_11_074325) do
+>>>>>>> 8a22f19 (LINE Pay available)
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,6 +133,33 @@ ActiveRecord::Schema.define(version: 2022_12_13_030057) do
     t.index ["booking_id"], name: "index_guest_reviews_on_booking_id"
 =======
 >>>>>>> a69000c (active storage install /add room relationship)
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
+    t.date "start_at", null: false
+    t.date "end_at", null: false
+    t.integer "price_per_night", null: false
+    t.integer "state", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.string "serial"
+    t.index ["room_id"], name: "index_bookings_on_room_id"
+    t.index ["slug"], name: "index_bookings_on_slug", unique: true
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
   create_table "rooms", force: :cascade do |t|
