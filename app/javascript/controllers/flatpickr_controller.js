b4a4f2368042f5a0d6efa19a0b32986a81272ba6
kerker000 <kate00715@gmail.com>
2022-12-15 18:06:56 +0800

    nights count wip

---
+++ app/javascript/controllers/flatpickr_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from 'stimulus';
+import { Application } from 'stimulus'
+import { definitionsFromContext } from 'stimulus/webpack-helpers'
+
+const application = Application.start()
+const context = require.context('../controllers', true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+// import Flatpickr
+import Flatpickr from 'stimulus-flatpickr'
+
+// Import style for flatpickr
+require("flatpickr/dist/flatpickr.css")
+
+// Manually register Flatpickr as a stimulus controller
+application.register('flatpickr', Flatpickr)
+
+export default class extends Controller {
+  static targets = ['output'];
+
+  connect() {
+    console.log("123")
+  }
+}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_14_154747) do
+ActiveRecord::Schema.define(version: 2022_12_15_063124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2022_12_14_154747) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "serial"
+    t.integer "headcount"
     t.index ["room_id"], name: "index_bookings_on_room_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
