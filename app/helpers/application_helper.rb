95213ea82df962f720b02beb430a2855e36f742c
Dengz <dengweiyi870626@gmail.com>
2022-12-15 17:59:09 +0800

    show page data import

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def equipment_provided
+    res=[]
+    if @room.has_wifi
+      res << "Wi-Fi"
+    end
+    if @room.has_kitchen
+      res << "廚房"
+    end
+    if @room.has_air_con
+      res << "空調"
+    end
+    if @room.has_bathtub
+      res << "浴缸"
+    end
+    res
+  end
 end
