d80aee697b951799dbb9d9f8cb0addcd16da60c3
kerker000 <kate00715@gmail.com>
2022-12-16 16:20:29 +0800

    booking card controller OK

--- app/javascript/controllers/booking_card_controller.js
+++ app/javascript/controllers/booking_card_controller.js
@@ -3,26 +3,61 @@ import dayjs from 'dayjs'
 
 export default class extends Controller {
 
-  static targets=['amount', "endDate", "startDate","nights"]
+  static targets = ['amount', 'endDate', 'startDate', 'nights', 'price', 'total']
+  static values = {
+    nights: Number,
+    price: Number,
+    amount: Number,
+    dateEnd: String,
+    dateStart: String,
+  }
 
   connect() {
-    let amount = this.amountTarget.value
-    
-  }
-  countDates(){
-    if ((this.endDateTarget.textContent !== null) && (this.startDateTarget.textContent !== null)){
-    }
+    this.price = Number(this.priceTarget.innerText.replace('$',''))
+    this.night = 1
+    this.amount = this.price * this.night
   }
 
   startDate(){
-    this.countDates()
+    this.dateStart = this.startDateTarget.value
+    if ((this.dateStart) && (this.dateEnd)){
+      this.changeNightsText()
+      this.changeAmountText()
+      this.changeTotalText()
+    }
   }
 
   endDate(){
-    this.countDates()
+    this.dateEnd = this.endDateTarget.value
+    if ((this.dateStart) && (this.dateEnd)){
+      this.changeNightsText()
+      this.changeAmountText()
+      this.changeTotalText()
+    }
   }
 
   dateDiff(dateStart, dateEnd) {
-    dayjs(dateEnd).diff(dayjs(dateStart), 'day')
+    return dayjs(dateEnd).diff(dayjs(dateStart), 'day')
   };
+
+  countNights(){
+    this.nights = this.dateDiff(this.dateStart, this.dateEnd)
+    return this.nights
+  }
+
+  changeNightsText(){
+    if (this.countNights() > 0 ){
+      this.nightsTarget.innerText = ` x ${this.countNights()} 晚`
+    }else{
+      this.nightsTarget.innerText = '請重新輸入'
+    }
+  }
+
+  changeAmountText(price,nights){
+    this.amount = this.price * this.nights
+    this.amountTarget.innerText = this.amount
+  }
+  changeTotalText(){
+    this.totalTarget.innerText = `$ ${this.amount} TWD`
+  }
 }
