066a6cb7323bfdedca2ba76fd3448907d12c7a99
kerker000 <kate00715@gmail.com>
2022-12-16 13:36:28 +0800

    no console.log

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -6,19 +6,9 @@ const application = Application.start()
 const context = require.context('../controllers', true, /\.js$/)
 application.load(definitionsFromContext(context))
 
-// import Flatpickr
 import Flatpickr from 'stimulus-flatpickr'
-
-// Import style for flatpickr
 require("flatpickr/dist/flatpickr.css")
-
-// Manually register Flatpickr as a stimulus controller
 application.register('flatpickr', Flatpickr)
-
 export default class extends Controller {
   static targets = ['output'];
-
-  connect() {
-    // console.log("123")
-  }
 }
