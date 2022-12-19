e6e652b259beb0a766400f277175a97797c52451
Dengz <dengweiyi870626@gmail.com>
2022-12-19 11:07:23 +0800

    Room card order by update_at

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   root "rooms#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: { sessions: "users/sessions", omniauth_callbacks: "users/omniauth_callbacks", passwords: "users/passwords", registrations: "users/registrations", confirmations: "users/confirmations", mailer: "users/mailer", unlocks: "users/unlocks" }
-  # root "room#home"
   resources :rooms do
     delete '/photos/:photo_id' => 'rooms#destroy_photo', as: :destroy_photo, on: :member
-    collection do
+    collection do 
       get 'manage'
+      get 'wish_list_rooms'
     end
-    member do 
+    member do
       patch :wish_list
+      
     end
   end
 
