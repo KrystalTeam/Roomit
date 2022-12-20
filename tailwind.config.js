f054dc7a72278ba5c01d1dd7e49d3f55344269bb
Dengz <dengweiyi870626@gmail.com>
2022-12-20 17:18:12 +0800

    Wish_list Show WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,37 @@
 module.exports = {
   purge: [
-    "./app/**/*.html.erb",
-    "./app/helpers/**/*.rb",
-    "./app/javascript/**/*.js",
-    "./app/javascript/**/*.vue",
+    './app/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/javascript/**/*.vue',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      xs: { min: "350px" },
-      sm: { min: "640px" },
-      md: { min: "768px" },
-      lg: { min: "1024px" },
-      xl: { min: "1200px" },
-      xxl: { min: "1400px" },
+      xs: { min: '350px' },
+      sm: { min: '640px' },
+      md: { min: '768px' },
+      lg: { min: '1024px' },
+      xl: { min: '1200px' },
+      xxl: { min: '1400px' },
     },
     container: {
       center: true,
     },
     extend: {
       minWidth: {
-        568: "568px",
+        568: '568px',
       },
       maxWidth: {
-        568: "568px",
+        568: '568px',
       },
       colors: {
-        fb: "#4267B2",
+        fb: '#4267B2',
       },
       spacing: {
-        500: "500px",
+        500: '500px',
+        600: '600px',
+        700: '700px',
       },
     },
   },
@@ -37,8 +39,8 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require("tailwindcss/plugin"),
-    require("@tailwindcss/aspect-ratio"),
-    require("tw-elements/dist/plugin"),
+    require('tailwindcss/plugin'),
+    require('@tailwindcss/aspect-ratio'),
+    require('tw-elements/dist/plugin'),
   ],
 };
