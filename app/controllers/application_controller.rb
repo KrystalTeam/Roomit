55b8c98e49c7bb17b1ad29aec9170f4063aadd18
kerker000 <kate00715@gmail.com>
2022-12-21 11:01:24 +0800

    fix pr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def should_compelete_user_info
-    unless current_user.name  && current_user.phone_num && current_user.name != '' && current_user.phone_num != ''
-        redirect_to edit_user_registration_path, alert: '需填寫完整資料'
+    if current_user.name.blank? && current_user.phone_num.blank?
+      redirect_to edit_user_registration_path, alert: '需填寫完整資料'
     end
   end
 end
