ce31291e4e7e3dc3202f27478849846018f06a83
Dengz <dengweiyi870626@gmail.com>
2022-12-26 21:36:24 +0800

    add state to user_edit_controller & fix booking_card_controller classfont-bold

--- app/javascript/controllers/user_edit_controller.js
+++ app/javascript/controllers/user_edit_controller.js
@@ -4,17 +4,24 @@ export default class extends Controller {
   static targets = ['edit', 'save', 'editTip', 'currentSetting'];
 
   connect() {}
-  input_switch(e) {
-    e.preventDefault();
-    if (e.srcElement.textContent === '取消') {
+  setEditStatus(state) {
+    if (state) {
       this.editTipTarget.classList.add('hidden');
       this.editTarget.classList.add('hidden');
       this.currentSettingTarget.classList.remove('hidden');
-      e.srcElement.textContent = '編輯';
     } else {
       this.editTipTarget.classList.remove('hidden');
       this.editTarget.classList.remove('hidden');
       this.currentSettingTarget.classList.add('hidden');
+    }
+  }
+  input_switch(e) {
+    e.preventDefault();
+    if (e.srcElement.textContent === '取消') {
+      this.setEditStatus(true);
+      e.srcElement.textContent = '編輯';
+    } else {
+      this.setEditStatus(false);
       e.srcElement.textContent = '取消';
     }
   }
