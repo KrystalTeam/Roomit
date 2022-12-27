453d4df8ea5c6b2ae8d447cf0df21cb5b92812e9
Dengz <dengweiyi870626@gmail.com>
2022-12-27 16:45:35 +0800

    remove useless gem and run rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ApplicationRecord
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
-      
+
       UserMailer.welcome(user).deliver_now
     end
   end
