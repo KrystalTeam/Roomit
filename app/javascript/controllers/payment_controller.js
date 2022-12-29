9784e688a9c2f95601f120597480c7dd5451b31b
allison <fidomoon@hotmail.com>
2022-12-30 00:18:49 +0800

    front get value

--- app/javascript/controllers/payment_controller.js
+++ app/javascript/controllers/payment_controller.js
@@ -1,15 +1,39 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  static targets = ["linePay", "ebPay"];
+  static targets = ["linePay", "ebPay", "MerchantOrderNo"];
+  connect() {
 
+  }
  
   btneb(){
-    this.changeState(true);    
+    this.changeState(true);
+    // e.peaventDefault();
+
+    var url_string = window.location.href; 
+    var url = new URL(url_string);
+    const id = url.searchParams.get("room_id");
+    const startTime = url.searchParams.get("start_at");
+    const endTime = url.searchParams.get("end_at");
+    const headcount = url.searchParams.get("headcount");
+
+    fetch(`/bookings/ebpay?room_id=${id}&start_at=${startTime}&end_at=${endTime}&headcount=${headcount}&commit=預定行程`, {
+      method: "GET"
+      },)
+      .then((resp) => resp.json())
+      .then(({ data }) => {
+        console.log(data.id);
+        this.MerchantOrderNoTarget.value = data.id;
+      })
+      .catch(() => {
+        console.log("error!!");
+      });    
   }
 
   btnline(){ 
     this.changeState(false);
+    
+
   }
   
   changeState(state){
