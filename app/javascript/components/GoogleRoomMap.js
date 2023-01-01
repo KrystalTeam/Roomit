2774ca2ea0f982b64c7ddaf3da084d26f3c04a76
Dengz <dengweiyi870626@gmail.com>
2023-01-01 20:21:17 +0800

    update wish_list_rooms MAP

--- app/javascript/components/GoogleRoomMap.js
+++ app/javascript/components/GoogleRoomMap.js
@@ -1,13 +1,13 @@
-import React from "react"
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api"
-import LocationButton from "./LocationButton"
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import LocationButton from './LocationButton';
 
 const taiwanBounds = {
   north: 28.17565,
   south: 19.17565,
   west: 116.9738819,
   east: 124.9738819,
-}
+};
 
 const mapOptions = {
   zoomControl: true,
@@ -15,59 +15,66 @@ const mapOptions = {
   mapTypeControl: false,
   fullscreenControl: false,
   restriction: {
-    latLngBounds: taiwanBounds
+    latLngBounds: taiwanBounds,
   },
-}
+};
 class GoogleRoomMap extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      map: ( /** @type google.maps.Map */ (null))
-    }
+      map: /** @type google.maps.Map */ (null),
+    };
   }
 
   render() {
-    let latAvg = this.props.data.map((datum => parseFloat(datum.lat))).reduce((a, b) => a + b, 0) / this.props.data.length
-    let lngAvg = this.props.data.map((datum => parseFloat(datum.lng))).reduce((a, b) => a + b, 0) / this.props.data.length
-    let center = {lat: latAvg, lng: lngAvg}
+    let latAvg =
+      this.props.data
+        .map((datum) => parseFloat(datum.lat))
+        .reduce((a, b) => a + b, 0) / this.props.data.length;
+    let lngAvg =
+      this.props.data
+        .map((datum) => parseFloat(datum.lng))
+        .reduce((a, b) => a + b, 0) / this.props.data.length;
+    let center = { lat: latAvg, lng: lngAvg };
 
     return (
-      <LoadScript
-        googleMapsApiKey={this.props.api_key}
-      >
-        <div
-          className="relative"
-        >
-          <GoogleMap 
-            mapContainerStyle={{width: '100%', height: '712px'}}
+      <LoadScript googleMapsApiKey={this.props.api_key}>
+        <div className='relative'>
+          <GoogleMap
+            mapContainerStyle={{
+              width: '100%',
+              height: `${window.screen.height - 236}px`,
+            }}
             center={center}
             zoom={7.8}
             options={mapOptions}
-            onLoad={map => this.setState({map: map})}
+            onLoad={(map) => this.setState({ map: map })}
           >
             <>
-            {/* Child components, such as markers, info windows, etc. */}
+              {/* Child components, such as markers, info windows, etc. */}
               {this.props.data.map((room) => {
                 return (
                   <Marker
                     key={room.id}
-                    position={{lat: parseFloat(room.lat), lng: parseFloat(room.lng)}}
+                    position={{
+                      lat: parseFloat(room.lat),
+                      lng: parseFloat(room.lng),
+                    }}
                   />
-                )
+                );
               })}
             </>
           </GoogleMap>
           <LocationButton
             onClick={() => {
-              this.state.map.panTo(center)
-              this.state.map.setZoom(7.8)
-              }
-            }
+              this.state.map.panTo(center);
+              this.state.map.setZoom(7.8);
+            }}
           />
         </div>
       </LoadScript>
-    )
+    );
   }
 }
 
-export default GoogleRoomMap
+export default GoogleRoomMap;
