94a128d03018f4a3baf9ee1f26c6f03218c5d7ea
kchsu <kevin3117213@gmail.com>
2023-01-03 01:02:33 +0800

    rooms/show Google Map w/ route calculation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
     Bullet.add_footer    = true
   end
 
+  config.hosts << /[a-z0-9-.]+\.ngrok\.io/
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
