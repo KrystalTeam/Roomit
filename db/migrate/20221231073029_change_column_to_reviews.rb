6778afdb2a45a8b635ea2edc99fe9ac666becd17
Dengz <dengweiyi870626@gmail.com>
2023-01-03 19:38:00 +0800

    remove change reviews migration

--- db/migrate/20221231073029_change_column_to_reviews.rb
+++
@@ -1,7 +0,0 @@
-class ChangeColumnToReviews < ActiveRecord::Migration[6.1]
-  def change
-    remove_reference :reviews, :user
-    add_reference :reviews, :booking, foreign_key: true
-    add_reference :reviews, :room, foreign_key: true
-  end
-end
